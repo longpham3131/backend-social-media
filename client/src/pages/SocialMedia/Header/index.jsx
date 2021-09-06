93a71ce3f34ad0ff359a7878196f28c233f35d81
longpham3131 <longpham3131@gmail.com>
2021-09-06 20:58:58 +0700

    notifications

--- client/src/pages/SocialMedia/Header/index.jsx
+++ client/src/pages/SocialMedia/Header/index.jsx
@@ -1,25 +1,33 @@
 import "./style.scss";
 import { Input } from "antd";
 import _defaultAvatar from "../../../assets/images/default-avatar.jpg";
+import { useState } from "react";
+import Notifications from "./Notifications";
 
 const { Search } = Input;
 const Header = () => {
   const onSearch = (value) => console.log(value);
+
   return (
     <div className="header">
       <div className="header__content">
         <div className="logo">Social</div>
         <div className="searchInput">
           <Search
-            placeholder="input search text"
+            placeholder="Tìm kiếm"
             onSearch={onSearch}
             style={{ width: 200 }}
           />
         </div>
         <div className="listTab">
-          <i class="fa fa-home"></i>
-          <i class="fa fa-comments "></i>
-          <i class="fa fa-heart"></i>
+          <div className="listTab__tabHome">
+            <i class="fa fa-home"></i>
+          </div>
+          <div className="listTab__tabMessages">
+            <i class="fa fa-comments "></i>
+          </div>
+
+          <Notifications />
 
           <img src={_defaultAvatar} alt="avatar" />
         </div>
