f5bbc22e4fcc92c495f963be7182f4b9d2c9fe79
longpham3131 <longpham3131@gmail.com>
2021-09-07 16:09:52 +0700

    update create post

---
+++ client/src/pages/SocialMedia/Body/ListPost/index.jsx
@@ -0,0 +1,76 @@
+import "./style.scss";
+import DefualtAvatar from "../../../../assets/images/default-avatar.jpg";
+import Dialog from "../../../../compoents/Dialog";
+import { useState } from "react";
+import { Select } from "antd";
+import { Input } from "antd";
+
+const { TextArea } = Input;
+
+const { Option } = Select;
+const ListPost = () => {
+  const [isShowDialog, setIsShowDialog] = useState(false);
+
+  const handleChange = (value) => {
+    console.log("VALUE SELCT", value);
+  };
+  return (
+    <div className="listPost">
+      {/* Post status */}
+      <div className="postStatus">
+        <img src={DefualtAvatar} alt="avatar" className="avatar" />
+        <div
+          className="postStatus__placehoder"
+          onClick={() => setIsShowDialog(true)}
+        >
+          <p> Chia sẽ với mọi người về suy nghĩ của bạn hiện tại nào...</p>
+        </div>
+        <Dialog
+          isShow={isShowDialog}
+          handleHideDialog={() => {
+            setIsShowDialog(false);
+          }}
+          btnSubmitName={"Đăng"}
+          title={"Đăng bài viết"}
+          content={
+            <div className="postStatus__content">
+              <div className="d-flex align-items-center">
+                <img
+                  src={DefualtAvatar}
+                  alt="avatar"
+                  className="avatar"
+                  style={{ paddingRight: "0" }}
+                />
+                <div>
+                  <p className="m-0">Phạm Hoàng Long</p>
+                  <Select
+                    defaultValue="public"
+                    style={{ width: 120 }}
+                    onChange={handleChange}
+                  >
+                    <Option value="public">Công khai</Option>
+                    <Option value="friends">Bạn bè</Option>
+                    <Option value="private">Chỉ mình tôi</Option>
+                  </Select>
+                </div>
+              </div>
+              <div className="pt-2">
+                <TextArea
+                  autoSize={{ minRows: 3 }}
+                  placeholder="Bạn đang nghĩ gì thế"
+                />
+              </div>
+              <p className="options">Tùy chọn thêm</p>
+              <div className="d-flex align-items-center justify-content-start">
+                <i class="fa fa-image"></i>
+                <i class="fa fa-tag"></i>
+              </div>
+            </div>
+          }
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ListPost;
--- client/src/pages/SocialMedia/index.jsx
+++ client/src/pages/SocialMedia/index.jsx
@@ -1,10 +1,13 @@
 import { useEffect } from "react";
 import Header from "./Header";
+import ListPost from "./Body/ListPost";
+import Body from "./Body";
 
 const SocialMedia = () => {
   return (
     <div>
       <Header />
+      <Body />
     </div>
   );
 };
