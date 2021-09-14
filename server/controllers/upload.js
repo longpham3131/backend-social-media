7d68fec4297d7349e97ad11fd1c561b080c066ba
sdkansdkan123 <sdkansdkan123@gmail.com>
2021-09-14 17:23:14 +0700

    cmt

---
+++ server/controllers/upload.js
@@ -0,0 +1,89 @@
+const SingleFile = require("../models/SingleFile");
+const MultipleFile = require("../models/MultipleFile");
+const { error500, error400 } = require("../util/res");
+const singleFileUpload = async (req, res, next) => {
+  try {
+    const file = new SingleFile({
+      fileName: req.file.originalname,
+      filePath: req.file.path.split('\\')[1],
+      fileType: req.file.mimetype,
+      fileSize: fileSizeFormatter(req.file.size, 2), // 0.00
+    });
+    await file.save();
+    console.log(file);
+    res.status(200).send({ message: "File upload successfully", data: file });
+  } catch (er) {
+    console.log(er);
+    res.status(400).send({ message: "error upload" });
+  }
+};
+
+const multipleFileUpload = async (req, res, next) => {
+  try {
+    let filesArray = [];
+    for (let i = 0; i < req.files.length; i++) {
+      const element = req.files[i];
+      const file = new SingleFile({
+        fileName: element.originalname,
+        filePath: element.path.split('\\')[1],
+        fileType: element.mimetype,
+        fileSize: fileSizeFormatter(element.size, 2), // 0.00
+      });
+      await file.save();
+      filesArray.push(file._id);
+    }
+    const multipleFiles = new MultipleFile({
+      title: req.body.title,
+      files: filesArray,
+    });
+    await multipleFiles.save();
+    res
+    .status(200)
+    .send({ message: "Files upload successfully", data: multipleFiles });
+  } catch (er) {
+    error400("upload error");
+  }
+};
+
+const getAllFiles = async (req, res, next) => {
+  try {
+    const files = await SingleFile.find();
+    res.json({
+      message: "success",
+      data: files,
+    });
+  } catch (er) {
+    error400("get file error");
+  }
+};
+const getAllMultiFiles = async (req, res, next) => {
+  try {
+    const files = await MultipleFile.find().populate('files');
+    console.log(files[0])
+    res.json({
+      message: "success",
+      data: files,
+    });
+  } catch (er) {
+    error400("get file error");
+  }
+};
+
+const fileSizeFormatter = (byte, decimal) => {
+  if (byte === 0) {
+    return "0 Bytes";
+  }
+  const dm = decimal || 2;
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "YB", "ZB"];
+  const index = Math.floor(Math.log(byte) / Math.log(1000));
+  return (
+    parseFloat((byte / Math.pow(1000, index)).toFixed(dm)) + "-" + sizes[index]
+  );
+};
+
+module.exports = {
+  singleFileUpload,
+  multipleFileUpload,
+  getAllFiles,
+  getAllMultiFiles,
+};
--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const authRouter = require("./routes/auth");
 const postRouter = require("./routes/post");
+const uploadRouter = require("./routes/upload");
 const helmet = require("helmet");
+const path = require('path')
 const cors = require("cors");
 require("dotenv").config();
 const connectDB = async () => {
@@ -29,7 +31,10 @@ app.use(cors());
 app.use(helmet());
 app.use("/api/auth", authRouter);
 app.use("/api/post", postRouter);
+app.use("/api/upload", uploadRouter.routes);
+app.use("/filemanager",express.static(path.join(__dirname,'uploads')))
 
-const PORT = 5000;
+
+const PORT = process.env.PORT|| 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
